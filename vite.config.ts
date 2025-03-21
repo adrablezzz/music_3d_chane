e72adf3a37d3f95ee2febe0749864659be7cda79
adrablezzz <1942775049@qq.com>
2025-03-21 17:43:00 +0800

    css fine

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /*
  * @Date: 2025-03-13 17:30:27
- * @LastEditTime: 2025-03-17 14:34:25
+ * @LastEditTime: 2025-03-21 17:32:01
  */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -8,4 +8,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  }
 })
