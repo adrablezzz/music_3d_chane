f0628d75661a539b6114e0b8e838d7ad9c34d322
adrablezzz <88357384+adrablezzz@users.noreply.github.com>
2025-03-21 16:24:27 +0800

    Add files via upload
    new

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+/*
+ * @Date: 2025-03-13 17:30:27
+ * @LastEditTime: 2025-03-17 14:34:25
+ */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
